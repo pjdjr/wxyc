5351fc10e600f401a45d2514a85e8b88610a078f
PJ <pjdisclafani@gmail.com>
2014-11-18 23:40:34 -0500

    first commit

---
+++ WXYC/src/org/wxyc/androidapp/ServiceHandler.java
@@ -0,0 +1,53 @@
+package org.wxyc.androidapp;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+ 
+public class ServiceHandler {
+ 
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
+ 
+    public ServiceHandler() {
+ 
+    }
+ 
+    /**
+     * Making service call
+     * @url - url to make request
+     * */
+    public String makeServiceCall(String url) {
+        try {
+            // http client
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpEntity httpEntity = null;
+            HttpResponse httpResponse = null;
+            HttpGet httpGet = new HttpGet(url); 
+            httpResponse = httpClient.execute(httpGet);
+            httpEntity = httpResponse.getEntity();
+            response = EntityUtils.toString(httpEntity);
+ 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+         
+        return response;
+ 
+    }
+}
